6894c8be5b1f74cdef9f3d61006d5babac6f0dae
Gia-17091 <haeunk0922@gmail.com>
2026-01-21 01:39:22 +0900

    [프로그래머스] L2 - 약수의 합 (#227)

---
+++ programmers/L2_기초/227_약수의_합.py
@@ -0,0 +1,15 @@
+# 약수의 합
+# 프로그래머스 L2 (기초)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12928
+# 알고리즘: 수학, 반복문
+# 작성자: 김하은
+# 작성일: 2026. 01. 21. 01:39:20
+
+def solution(n):
+    answer = 0
+    for x in range(1,n+1):
+        if n %x ==0:
+            answer+=x
+        else:
+            continue
+    return answer
