71627b6b0ef7a859cda00e538ed930c32c427c43
Gia-17091 <haeunk0922@gmail.com>
2026-01-20 10:20:13 +0900

    Add alternative array reversal solution
    Added an alternative solution using the reverse method.

--- programmers/L0_기초입문/입문/013_배열_뒤집기.py
+++ programmers/L0_기초입문/입문/013_배열_뒤집기.py
@@ -5,9 +5,11 @@
 # 작성자: 김하은
 # 작성일: 2026. 01. 20. 10:19:05
 
+# 처음 생각한 풀이
 def solution(num_list):
     return num_list[::-1]
 
+# 다른 방식을 찾아본 풀이
 def solution(num_list):
     num_list.reverse()
     return num_list
