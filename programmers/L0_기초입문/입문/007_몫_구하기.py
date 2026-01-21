59207579ca5aaf22d29086a66aaa33889541ea2d
Gia-17091 <haeunk0922@gmail.com>
2026-01-21 17:03:25 +0900

    [프로그래머스] L0 - 몫 구하기 (#7)

--- programmers/L0_기초입문/입문/007_몫_구하기.py
+++ programmers/L0_기초입문/입문/007_몫_구하기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120805
 # 알고리즘: 기초
 # 작성자: 김하은
-# 작성일: 2026. 01. 20. 09:35:39
+# 작성일: 2026. 01. 21. 17:03:23
 
 def solution(num1, num2):
     answer = num1 // num2
