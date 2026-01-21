145ec4e48d510b2f06bf51f81ab6ef7b201afeaf
Gia-17091 <haeunk0922@gmail.com>
2026-01-22 01:18:21 +0900

    Improve triangle condition check formatting
    Refactor triangle condition check for better readability.

--- programmers/L0_기초입문/입문/020_삼각형의_완성조건__1_.py
+++ programmers/L0_기초입문/입문/020_삼각형의_완성조건__1_.py
@@ -13,3 +13,26 @@ def solution(sides):
         return 1
     else:
         return 2
+
+"""
+공부 내용
+if (a+b) > c 
+    and (b+c) > a 
+    and (c+a) > b:
+    return 1
+이것을 Python에서는 "문장 끝"으로 인식한다.
+고로, 
+#1 괄호로 묶기
+if (
+    (a+b) > c 
+    and (b+c) > a 
+    and (c+a) > b
+    ):
+    return 1
+#2 역슬래시 사용
+if (a+b) > c \
+   and (b+c) > a \
+   and (c+a) > b:
+    return 1
+의 조치가 필요하며, #1이 실무 및 리뷰에서 권장하는 방식이다.
+"""
