363605e401dbc45304c7583b5bf3c81d9902ca03
Gia-17091 <haeunk0922@gmail.com>
2026-01-23 17:05:00 +0900

    [프로그래머스] L0 - 자릿수 더하기 (#35)

---
+++ programmers/L0_기초입문/입문/035_자릿수_더하기.py
@@ -0,0 +1,12 @@
+# 자릿수 더하기
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120906
+# 알고리즘: 기초
+# 작성자: 김하은
+# 작성일: 2026. 01. 23. 17:04:59
+
+def solution(n):
+    total = 0
+    for i in str(n):
+        total += int(i)
+    return total
