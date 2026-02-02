c6f6f60e9800bb281c71e9e7703d4e2d3025913c
Gia-17091 <haeunk0922@gmail.com>
2026-02-03 05:25:05 +0900

    [프로그래머스] L0 - 주사위의 개수 (#45)

---
+++ programmers/L0_기초입문/입문/045_주사위의_개수.py
@@ -0,0 +1,14 @@
+# 주사위의 개수
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120845
+# 알고리즘: 기초
+# 작성자: 김하은
+# 작성일: 2026. 02. 03. 05:25:04
+
+def solution(box, n):
+    answer = 1
+    
+    for length in box:
+        answer *= length // n
+        
+    return answer
