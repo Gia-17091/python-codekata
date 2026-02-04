e4c56d55cb0e3d2b748f16d252380cb9dd451b0f
Gia-17091 <haeunk0922@gmail.com>
2026-02-05 04:47:07 +0900

    [프로그래머스] L0 - 대문자와 소문자 (#54)

---
+++ programmers/L0_기초입문/입문/054_대문자와_소문자.py
@@ -0,0 +1,10 @@
+# 대문자와 소문자
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120893
+# 알고리즘: 기초
+# 작성자: 김하은
+# 작성일: 2026. 02. 05. 04:47:03
+
+def solution(my_string):
+    return my_string.swapcase()
+#swapcase() : 문자열에 있는 모든 문자의 대/소문자 반대로 변환
