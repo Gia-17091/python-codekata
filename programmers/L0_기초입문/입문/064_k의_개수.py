1284f7706e54a9ddb3ce1433197fa39a172a2148
Gia-17091 <haeunk0922@gmail.com>
2026-02-07 15:31:40 +0900

    [프로그래머스] L0 - k의 개수 (#64)

---
+++ programmers/L0_기초입문/입문/064_k의_개수.py
@@ -0,0 +1,12 @@
+# k의 개수
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120887
+# 알고리즘: 기초
+# 작성자: 김하은
+# 작성일: 2026. 02. 07. 15:31:39
+
+def solution(i, j, k):
+    answer = 0
+    for n in range(i, j+1):
+        answer+=str(n).count(str(k))
+    return answer
