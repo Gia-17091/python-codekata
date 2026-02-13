cc5aed68e4b393278de60630a45708aacc300f5f
Gia-17091 <haeunk0922@gmail.com>
2026-02-13 12:53:56 +0900

    [프로그래머스] L0 - 모스부호 (1) (#62)

--- programmers/L0_기초입문/입문/062_모스부호__1_.py
+++ programmers/L0_기초입문/입문/062_모스부호__1_.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120838
 # 알고리즘: 기초
 # 작성자: 김하은
-# 작성일: 2026. 02. 13. 12:53:45
+# 작성일: 2026. 02. 13. 12:53:54
 
 def solution(letter):
     answer = ''
