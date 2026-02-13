7e9742705a7e8b31fff307a5a6a33504c8ebf870
Gia-17091 <haeunk0922@gmail.com>
2026-02-13 12:53:23 +0900

    [프로그래머스] L0 - 모스부호 (1) (#62)

---
+++ programmers/L0_기초입문/입문/062_모스부호__1_.py
@@ -0,0 +1,20 @@
+# 모스부호 (1)
+# 프로그래머스 L0 (기초·입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120838
+# 알고리즘: 기초
+# 작성자: 김하은
+# 작성일: 2026. 02. 13. 12:53:20
+
+def solution(letter):
+    answer = ''
+    morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    trans_letter = letter.split(' ')
+    for char in trans_letter:
+        answer+= morse[char]
+    return answer
