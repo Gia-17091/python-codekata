dd54e0b6373511163e6e824042080852ed705e55
Gia-17091 <haeunk0922@gmail.com>
2026-02-24 12:24:19 +0900

    [프로그래머스] L0 - 한 번만 등장한 문자 (#65)

--- programmers/L0_기초입문/입문/065_한_번만_등장한_문자.py
+++ programmers/L0_기초입문/입문/065_한_번만_등장한_문자.py
@@ -3,18 +3,21 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120896
 # 알고리즘: 기초
 # 작성자: 김하은
-# 작성일: 2026. 02. 24. 12:18:56
+# 작성일: 2026. 02. 24. 12:24:17
 
 def solution(s):
     counts = {}
     for ch in s:
         if ch in counts:
-            counts[ch]+=1
+            counts[ch] += 1
         else:
-            counts[ch]=1
-    one_times = [
-        ch for ch, count in counts.items() if count ==1
-    ]
+            counts[ch] = 1
+
+    one_times = []
+    for ch, count in counts.items():
+        if count == 1:
+            one_times.append(ch)
+
     one_times.sort()
     answer = ''.join(one_times)
     return answer
