c6838d0f3a0d501f33b89c52fde4da0b89a4f80e
Gia-17091 <haeunk0922@gmail.com>
2026-02-27 12:20:00 +0900

    [프로그래머스] L2 - 나누어 떨어지는 숫자 배열 (#238)

--- programmers/L2_기초/238_나누어_떨어지는_숫자_배열.py
+++ programmers/L2_기초/238_나누어_떨어지는_숫자_배열.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12910
 # 알고리즘: 배열, 정렬
 # 작성자: 김하은
-# 작성일: 2026. 02. 27. 12:19:49
+# 작성일: 2026. 02. 27. 12:19:58
 
 def solution(arr, divisor):
     answer = []
