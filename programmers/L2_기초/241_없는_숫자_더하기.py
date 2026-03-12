5d8a39f79283ee0a4d6da64e06b1a800171d24fd
Gia-17091 <haeunk0922@gmail.com>
2026-03-12 20:58:32 +0900

    [프로그래머스] L2 - 없는 숫자 더하기 (#241)

---
+++ programmers/L2_기초/241_없는_숫자_더하기.py
@@ -0,0 +1,9 @@
+# 없는 숫자 더하기
+# 프로그래머스 L2 (기초)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/86051
+# 알고리즘: 배열, 해시
+# 작성자: 김하은
+# 작성일: 2026. 03. 12. 20:58:29
+
+def solution(numbers):
+    return 45 -sum(numbers)
